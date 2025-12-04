f81ef990db3c9df21900edf5eff3c99691eda7c7
0x1Jar <fajar3842@gmail.com>
2025-12-04 10:08:17 +0700

    add function get funds

---
+++ foundMe.sol
@@ -0,0 +1,19 @@
+//Get funds from users
+//withdraw funds
+//Set a minimum funding value in USD
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract FoundMe {
+    function funds( ) public {
+        //allow users to send $
+        // Have a minimum $ to sent
+        //. 1. How do we send ETH to this contract?
+
+        
+    }
+
+    // function withdraw() public {}
+}
