8e79202d820cc76bf708f0ff87b1e857e05b1271
0x1Jar <fajar3842@gmail.com>
2025-12-05 16:53:00 +0700

    add function withdraw

--- foundMe.sol
+++ foundMe.sol
@@ -15,15 +15,40 @@ contract FoundMe {
     address[] public  founders;
     mapping (address => uint256 amountFounded) public addressToAmountFounded;
 
+    function callMeRightAway() public {
+        
+    }
+
     function funds( ) public payable {
         // msg.value.getConversionRate();
         require(msg.value.getConversionRate() >= minumumUsd, "didn't send enough ETH"); //1e18
         founders.push(msg.sender);
-        addressToAmountFounded[msg.sender] = addressToAmountFounded[msg.sender] + msg.value;
+        addressToAmountFounded[msg.sender] += msg.value;
 
     }
-    // What is a reverst?
-    // Undo any actions that have been done, and send the remining gas back
 
-    // function withdraw() public {} 
+    function withdraw() public {
+        //for loop
+        //[1, 2, 3,] elements 
+        //0, 1, 2, 3 indexes
+        // for (/* start index, ending index,  step amount*/)
+        for (uint256 founderIndex = 0 ; founderIndex < founders.length; founderIndex++){
+            address funder = founders[founderIndex];
+            addressToAmountFounded[funder] = 0;
+        }
+        //reset the aray
+        founders = new address[](0);
+        // actually withdraw the funds
+
+        //not recomeded using trasnfer and send!
+        //transfer
+        // payable(msg.sender).transfer(address(this).balance);
+        // //send
+        // bool sendSuccess = payable (msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send Failed");
+        //call
+        (bool callSuccess, ) = payable (msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "call failed");
+        
+    } 
 }
